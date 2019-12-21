918e759fb4d1bf6cb474f469e5080cb7e2a22f1d
namizato <namizato@sinq.co.jp>
2019-12-21 16:22:37 +0900

    first commit

---
+++ 03_if.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+var example = []string{
+	"golang",
+	"hands-on",
+	"in",
+	"kagawa",
+}
+
+func main() {
+	var data []string
+	data = example
+	for _, v := range data {
+		fmt.Println(v)
+		if v == "in" {
+			fmt.Println("○")
+		} else {
+			fmt.Println("×")
+		}
+	}
+}
---
+++ 04_stdio.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+var example = []string {
+	"golang",
+	"hands-on",
+	"in",
+	"kagawa",
+}
+
+func main() {
+	var data []string
+	data = example
+	for _, v := range data {
+		fmt.Println(v)
+		fmt.Print("input: ")
+		var ans string
+		fmt.Scanln(&ans)
+
+		if v == ans {
+			fmt.Println("○")
+		} else {
+			fmt.Println("×")
+		}
+	}
+}
